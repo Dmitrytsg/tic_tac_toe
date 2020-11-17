fb9bb8a238bb5fe622ac72154b43cbebb5e43b3d
Dmitrytsg <dmitrytsg@bk.ru>
2020-11-18 00:56:43 +0300

    ver 1.1.3

--- TTT_VS_AI.py
+++ TTT_VS_AI.py
@@ -3,12 +3,21 @@ import random
 
 def main():
     global a
-    a = [["_"]*3 for i in range(3)]
 
+    per = 0
+    m = "start"
+    print(colored("первыми ходят x","magenta",attrs=['bold']))
+    print((colored("Для того чтобы выбрать x введите [1]","magenta",attrs=['bold'])))
+    print((colored("Для того чтобы выбрать o введите [2]","magenta",attrs=['bold'])))
+    n = int(input())
+    if n == 1: 
+        m = "x"
+        per += 1
+    if n == 2: m = "o"
+    a = [["_"]*3 for i in range(3)]
     for i in a:
         print(*i)
-    m = "start"
-    per = 0
+    
     flag = True
     br = True
     while flag and br:
@@ -16,9 +25,8 @@ def main():
         sub = True
         if per%2 == 0:
             cprint('----Ход ИИ----','blue') 
-            AI_move(per)
-        if per%2 == 1: 
-            m = 'o'
+            AI_move(per,m)
+        if per%2 == 1:
             while sub:
                 print("Куда хотите поставить",m,':')
                 i,j = map(int, input().split())
@@ -75,60 +83,70 @@ def scan_line(e1,e2,e3):
         result = True
     return result
 
-def AI_move(per:int):
+def AI_move(per:int,m:str):
     """
     -Обработка поля программой 
     -Принимает шаг
     -Возвращает изменённое поле
-    param a: array
     param per: move count
+    param m: противник
     """
+    print(per,m)
+    value = "start" #значение ИИ в зависимости от параметра m
     #Первые 2 хода обрабатываем вручную, они ключевые
-    if per == 0:
-        a[0][0] = 'x'
-        return
-    elif per == 2:
-        if a[2][0] == '_':
-            a[2][0] = 'x'
-        elif a[2][2] == '_':
-            a[2][2] = 'x'
-        return
+    if m == "o":
+        value = "x"
+        if per == 0:
+            a[0][0] = "x"
+            return
+        elif per == 2:
+            if a[2][0] == '_':
+                a[2][0] = 'x'
+            elif a[2][2] == '_':
+                a[2][2] = 'x'
+            return
+    if m == "x":
+        value = "o"
+        if per == 2:
+            if a[1][1] == "_": a[1][1] = "o"
+            else: a[0][0] = "o"
+            return
     #ходы дальше обрабатываются автоматически
-    else:
-        #обрабатываем критическую ситуацию(2 'x')
+    if per >= 4:
+        #обрабатываем критическую ситуацию(2 value)
         for i in range(3): #проверяем строки
-            if scan_to_win([i,0],[i,1],[i,2],"x"):
+            if scan_to_win([i,0],[i,1],[i,2],value,value):
                 return
         for i in range(3): #проверяем столбцы
-            if scan_to_win([0,i],[1,i],[2,i],'x'):
+            if scan_to_win([0,i],[1,i],[2,i],value,value):
                 return
-        if scan_to_win([0,0],[1,1],[2,2],'x'):#проверяем главную диагональ
+        if scan_to_win([0,0],[1,1],[2,2],value,value):#проверяем главную диагональ
             return
-        if scan_to_win([0,2],[1,1],[2,0],'x'):#проверяем побочную диагональ
+        if scan_to_win([0,2],[1,1],[2,0],value,value):#проверяем побочную диагональ
             return
 
-        #обрабатываем критическую ситуацию(2 'o')
+        #обрабатываем критическую ситуацию(2 m)
         for i in range(3): #проверяем строки
-            if scan_to_win([i,0],[i,1],[i,2],"o"):
+            if scan_to_win([i,0],[i,1],[i,2],m,value):
                 return
         for i in range(3): #проверяем столбцы
-            if scan_to_win([0,i],[1,i],[2,i],'o'):
+            if scan_to_win([0,i],[1,i],[2,i],m,value):
                 return
-        if scan_to_win([0,0],[1,1],[2,2],'o'):#проверяем главную диагональ
+        if scan_to_win([0,0],[1,1],[2,2],m,value):#проверяем главную диагональ
             return
-        if scan_to_win([0,2],[1,1],[2,0],'o'):#проверяем побочную диагональ
+        if scan_to_win([0,2],[1,1],[2,0],m,value):#проверяем побочную диагональ
             return
 
-        #проверяем случай когда 1 'x' и остальное пусто
+        #проверяем случай когда 1 value и остальное пусто
         for i in range(3): #проверяем строки
-            if scan_to_move([i,0],[i,1],[i,2],"x"):
+            if scan_to_move([i,0],[i,1],[i,2],value,value):
                 return
         for i in range(3): #проверяем столбцы
-            if scan_to_move([0,i],[1,i],[2,i],'x'):
+            if scan_to_move([0,i],[1,i],[2,i],value,value):
                 return
-        if scan_to_move([0,0],[1,1],[2,2],'x'):#проверяем главную диагональ
+        if scan_to_move([0,0],[1,1],[2,2],value,value):#проверяем главную диагональ
             return
-        if scan_to_move([0,2],[1,1],[2,0],'x'):#проверяем побочную диагональ
+        if scan_to_move([0,2],[1,1],[2,0],value,value):#проверяем побочную диагональ
             return
 
         #во всех остальных случаях значение ставится рандомно
@@ -136,47 +154,50 @@ def AI_move(per:int):
             i = random.randint(0, 2)
             j = random.randint(0, 2)
             if a[i][j] == '_':
-                a[i][j] = 'x'
+                a[i][j] = value
                 break
 
 
-def scan_to_move(e1,e2,e3,s):#!!!!!!!!!!!! можно сделать рандом !!!!!!!!!!!!
+def scan_to_move(e1,e2,e3,s,m):#!!!!!!!!!!!! можно сделать рандом !!!!!!!!!!!!
     """
     -Проверяет ситуацию вида x__
     -Сканирует заданную линию на поле
     -Если ситуация, то делает ход
     -Возвращает флаг 
+    -param s: значение которое ищем
+    -param m: значение ИИ
     """
     result = False
     if a[e1[0]][e1[1]] == s and a[e2[0]][e2[1]] == '_' and a[e3[0]][e3[1]] == '_':
-        a[e3[0]][e3[1]] = 'x'
+        a[e3[0]][e3[1]] = m
         result = True
     if a[e1[0]][e1[1]] == '_' and a[e2[0]][e2[1]] == s and a[e3[0]][e3[1]]  == '_':
-        a[e3[0]][e3[1]] = 'x'
+        a[e3[0]][e3[1]] = m
         result = True
     if a[e1[0]][e1[1]] == '_' and a[e2[0]][e2[1]] == '_' and a[e3[0]][e3[1]] == s:
-        a[e1[0]][e1[1]] = 'x'
+        a[e1[0]][e1[1]] = m
         result = True
     return result
 
-def scan_to_win(e1,e2,e3,s):
+def scan_to_win(e1,e2,e3,s,m):
     """
     -Проверяет ситуацию вида oo_ или xx_
     -Сканирует заданную линию на поле
     -Если ситуация "критическая", то делает ход
-    -Возвращает флаг 
+    -Возвращает флаг
+    -param s: значение которое ищем
+    -param m: значение ИИ 
     """
     result = False
     if a[e1[0]][e1[1]] == s and a[e2[0]][e2[1]] == s and a[e3[0]][e3[1]] == '_':
-        a[e3[0]][e3[1]] = 'x'
+        a[e3[0]][e3[1]] = m
         result = True
     if a[e1[0]][e1[1]] == s and a[e2[0]][e2[1]] == '_' and a[e3[0]][e3[1]] == s:
-        a[e2[0]][e2[1]] = 'x'
+        a[e2[0]][e2[1]] = m
         result = True
     if a[e1[0]][e1[1]] == '_' and a[e2[0]][e2[1]] == s and a[e3[0]][e3[1]] == s:
-        a[e1[0]][e1[1]] = 'x'
+        a[e1[0]][e1[1]] = m
         result = True
     return result
 
 main()
-
